05048d6c45a459df917d10f1bfff9fafd95f33e6
Alice Post <alicep2322@gmail.com>
2019-10-11 09:40:11 -0600

    Add average_age method to astronaut model

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -3,4 +3,8 @@ class Astronaut < ApplicationRecord
   has_many :missions, through: :astronaut_missions
 
   validates_presence_of :name, :age, :job
+
+  def self.average_age
+    Astronaut.average(:age).round(0)
+  end
 end
