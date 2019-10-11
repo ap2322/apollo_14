cfad8ec901100f50a962c8ebb967993ba9660881
Alice Post <alicep2322@gmail.com>
2019-10-11 09:30:32 -0600

    Add '/astronauts' index with route and controller'

---
+++ spec/features/astronaut/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "On the /astronauts index" do
+  it "should show a list of astronauts with their
+      - Name
+      - Age
+      - Job " do
+
+    armstrong = Astronaut.create(name: 'Neil Armstrong', age: 37, job: 'Commander')
+    aldrin = Astronaut.create(name: 'Buzz Aldrin', age: 39, job: 'NASA Astronaut' )
+    ride = Astronaut.create(name: 'Sally Ride', age: 31, job: 'NASA Astronaut' )
+
+    visit '/astronauts'
+
+    expect(page).to have_content(armstrong.name)
+    expect(page).to have_content(armstrong.age)
+    expect(page).to have_content(armstrong.job)
+    expect(page).to have_content(aldrin.name)
+    expect(page).to have_content(aldrin.age)
+    expect(page).to have_content(aldrin.job)
+    expect(page).to have_content(ride.name)
+    expect(page).to have_content(ride.age)
+    expect(page).to have_content(ride.job)
+  end
+end
